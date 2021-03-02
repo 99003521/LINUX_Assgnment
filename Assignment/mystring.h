22f63238d489490e7ee61f5e2c98628d17935de7
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-03-02 08:35:48 +0530

    strFun

--- Assignment/mystring.h
+++ Assignment/mystring.h
@@ -2,20 +2,5 @@
 #define __MYSTRING_H
 // mystrlen, mystrcpy, mystrcat, mystrcmp  
 
-char *substring(int i,int j,char *ch)
-{
-    int n,k=0;
-    char *ch1;
-    ch1=(char*)malloc((j-i+1)*1);
-    n=j-i+1;
-
-    while(k<n)
-    {
-        ch1[k]=ch[i];
-        i++;k++;
-    }
-
-    return (char *)ch1;
-}
 
 #endif
