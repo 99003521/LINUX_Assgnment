5679a4a65ff963e82718c26ce0f9fa4ce2bd8613
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-03-02 08:36:03 +0530

    strfun

--- Assignment/mystring.c
+++ Assignment/mystring.c
@@ -1,27 +1,53 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
 
-char *mystrcat(char* s1,char* s2){
-    int length, j;
+char *mystrcpy(char *a, char *b){
+  char text1[MAX_SIZE] = a, text2[MAX_SIZE] = b;
 
-  // store length of s1 in the length variable
-  length = 0;
-  while (s1[length] != '\0') {
-    ++length;
-  }
+  /* Input original string from user */
+  printf("Enter any string: ");
+  gets(text1);
 
-  // concatenate s2 to s1
-  for (j = 0; s2[j] != '\0'; ++j, ++length) {
-    s1[length] = s2[j];
-  }
+  /* Copy text1 to text2 using strcpy() */
+  strcpy(text2, text1);
 
-  // terminating the s1 string
-  s1[length] = '\0';
-
-  printf("After concatenation: ");
-  puts(s1);
-
-  return (char*) s1;
+  printf("First string = %s\n", text1);
+  printf("Second string = %s\n", text2);
+  return text2;
 }
-int main(){
-    my
+
+char *mystrcat(char *a, char *b){
+  char destination[] = a;
+  char source[] = b;
+  strcat(destination,source);
+  printf("Concatenated String: %s\n", destination);
+  char *ans = destination;
+  return (char *) ans;
+}
+
+int mystrlen(char *a){
+  char s[] = a;
+  int i;
+  for (i = 0; s[i] != '\0'; ++i);
+  printf("Length of the string: %d", i);
+  return i;
+}
+
+int mystrcmp(char *a,char *b){
+  // return strcmp(a,b);
+  int flag=0,i=0;  // integer variables declaration  
+  while(a[i]!='\0' &&b[i]!='\0')  // while loop  
+  {  
+    if(a[i]!=b[i])  
+    {  
+      flag=1;  
+      break;  
+    }  
+    i++;  
+  }  
+  if(flag==0)  
+    return 0;  
+  else  
+    return 1;  
 }
